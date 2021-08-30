30432f94e6829275f3595370366ebe4182962f22
Gabriel Turlea <gabrielturlea27@gmail.com>
2021-08-31 01:19:50 +0300

    layout update

--- assets/js/main-v1.js
+++ assets/js/main-v1.js
@@ -4,11 +4,6 @@ let submenuItems = document.querySelectorAll('.submenu');
 let clickCheckMenu = null;
 
 document.addEventListener('click', function(e){
-    // if (!document.getElementById('l2').contains(e.target) && (!document.getElementById('logo-menu').contains(e.target))){
-    //     alert("Clicked outside l2 and logo-menu");
-    //      document.getElementById('l2').style.height="0px"; //the same code you've used to hide the menu
-    //   } 
-   
     let parentNode = e.target.parentNode;
     if(parentNode.classList){
    
@@ -46,7 +41,7 @@ menuItems.forEach(function(f){
 })
 
 
-
+// mobile btn
 hamburger.addEventListener('click', function(){
     navbarUl = document.querySelector('#navbar-ul');
     socialmedia = document.querySelector('#socialmedia');
@@ -60,80 +55,129 @@ hamburger.addEventListener('click', function(){
     }
 })
 
-// let main = document.querySelector('#main');
-// // let mainMatrix = document.querySelector('#main-matrix');
-// let matrixSpans = document.querySelectorAll('.terminal');
-// let overlay = document.querySelector('#overlay');
-// let checkboxes = document.querySelectorAll('input[type=checkbox]');
-// var svgimages = document.querySelectorAll('.svg-image');
+//upscreen
+const upScreen = document.querySelector('#up-screen')
+window.addEventListener('scroll', function(e) {
+    y = window.scrollY;
+    if(y > 150){
+        upScreen.style.display = 'initial';
 
-// document.addEventListener("DOMContentLoaded", initialanimation);
+    }else{
+        upScreen.style.display = 'none'
+    }
+})
+upScreen.addEventListener("click", function(){
+    document.querySelector('html').scrollTo({
+        top: 0,
+        behavior: "smooth"
+        })
+})
+
+// initialize homeinitial (where all the functions are running) after the page loads
+document.addEventListener("DOMContentLoaded", homeinitial);
 
 
+function homeinitial(){
+    // home carousel
+    let banner = document.querySelector("#banner")
+    let homecarouselindex = 0;
+    let homeleftarrow = banner.querySelector('.carousel-arrow-left');
+    let homerightarrow= banner.querySelector('.carousel-arrow-right');
+    let homeslider = banner.querySelectorAll('.carousel-slide');
+    let homebullet = banner.querySelectorAll('.carousel-bullets');
+    
+    const firstBulletColor = homebullet[0].style;
+    console.log(firstBulletColor)
+    homeleftarrow.addEventListener('click', function(){
 
-//  function initialanimation(){
+        clearInterval(chimageinterval);
+        changeimage(1);
+        chimageinterval = setInterval(changeimage, 5000)
+        
+    })
+        
+    homerightarrow.addEventListener('click', function(){
 
-//   window.addEventListener('load', function(f){
-//     //display initial homepage
-//       tl = gsap.timeline({ defaults:{duration: .5}})
-//       tl.to(main, { ease: "expo", opacity: 1});
-
-//     //display monitor animation
-//     let imageindex = 0;
-//     monitoranimation();
+        clearInterval(chimageinterval);
+        changeimage();
+        chimageinterval = setInterval(changeimage, 5000)
+    })
 
 
-//     function monitoranimation(){
+    f = homeslider[homecarouselindex];
 
-//         f = svgimages[imageindex];
-//       console.log(f)
-//         chimageinterval = setInterval(changeimage, 5000)
+    chimageinterval = setInterval(changeimage, 5000)
 
-//         function changeimage(f){
-            
-//             let elem = svgimages[imageindex];
-//             if(f){
-//                 if(imageindex == 0)
-//                 {
-//                     let svgimageslength = svgimages.length;
-//                     // let svgimageslenghtminus = svgimageslength - 1
-                  
-//                     next = svgimages[svgimageslength - 1]
+    function changeimage(f){
+
+        let elem = homeslider[homecarouselindex];
+        if(f){
+            if(homecarouselindex == 0)
+            {
+                let homesliderlength = homeslider.length;
+                // let homesliderlenghtminus = homesliderlength - 1
               
-//                     imageindex = svgimageslength  -1;
+                next = homeslider[homesliderlength - 1]
+                bullet = homebullet[homesliderlength - 1]
 
-                  
-//                 }else{
-//                     next = svgimages[+imageindex-1]
-//                     imageindex = +imageindex -1  
-//                 }
-            
+                homecarouselindex = homesliderlength  -1;
 
-//             }else{
-//                 if(svgimages.length == +imageindex + 1)
-//                 {
-//                     next = svgimages[0]
-//                     imageindex = 0;
-//                 }else{
-//                     next = svgimages[+imageindex+1]
-//                     imageindex = +imageindex +1  
-//                 }
-//             }
-//             carouselanimation()
-          
-//             function carouselanimation(){
-//                 tl = gsap.timeline({ defaults:{duration: 1}})
-//                 tl.to(elem, { ease: "power2", opacity: 0})
-//                 tl.to(next, { ease: "power2", opacity: 1, delay: -.9})
-//             }
+              
+            }else{
+                next = homeslider[+homecarouselindex-1]
+                bullet = homebullet[+homecarouselindex-1]
+                homecarouselindex = +homecarouselindex -1  
+            }
+         
 
-//         }
-//       }         
-//   });
+        }else{
+            if(homeslider.length == +homecarouselindex + 1)
+            {
+                next = homeslider[0]
+                bullet = homebullet[0]
+
+                homecarouselindex = 0;
+            }else{
+                next = homeslider[+homecarouselindex+1]
+                bullet = homebullet[+homecarouselindex+1]
+
+                homecarouselindex = +homecarouselindex +1  
+            }
+        }
+        //clear indexes to prevent overlapping
+        clearcarouselindexes()
+
+        //start carousel animation
 
         
+        carouselanimation()
 
-// }
+        homebullet.forEach(function(h){
+            h.style.backgroundColor = "white";
+        })
+        bullet.style.backgroundColor = "#838383";
+
+        function clearcarouselindexes(){
+            homeslider.forEach(function(e){
+                e.style.zIndex = "9";
+            })
+        }
+
+        function carouselanimation(){
+            // setTimeout(function(){
+            //     elem.style.opacity = 0;
+            // }, 300)
+            
+            // next.style.opacity = 1;
+            next.style.zIndex = 10;
+        
+            tl = gsap.timeline({ defaults:{duration: 1}})
+            tl.to(elem, { ease: "power2", opacity: 0})
+            tl.to(next, { ease: "power2", opacity: 1, delay:-.9})
+         
+        }
+    }
+}
 
 //  checkboxes.forEach(function(e){
 //    e.addEventListener('click', function(){
